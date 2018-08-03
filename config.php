9a78b03311aa330577cb46714b42eca08fb6fccf
godar <info@tropotek.com>
2018-08-03 11:02:57 +1000

    ~Auto: Commit

--- config.php
+++ config.php
@@ -10,15 +10,18 @@ if ($composer) {
 $routes = $config->getRouteCollection();
 if (!$routes) return;
 
-$params = array('role' => 'admin');
+
+$params = array();
+
+//$params = array('role' => 'admin');
 //$routes->add('Rules Admin Settings', new \Tk\Routing\Route('/ruleset/adminSettings.html', 'Rs\Controller\SystemSettings::doDefault', $params));
 
-$params = array('role' => array('admin', 'client'));
+//$params = array('role' => array('admin', 'client'));
 //$routes->add('Rules Institution Settings', new \Tk\Routing\Route('/ruleset/institutionSettings.html', 'Rs\Controller\InstitutionSettings::doDefault', $params));
 $routes->add('client-rule-manager', new \Tk\Routing\Route('/client/ruleManager.html', 'Rs\Controller\RuleManager::doDefault', $params));
 $routes->add('client-rule-edit', new \Tk\Routing\Route('/client/ruleEdit.html', 'Rs\Controller\RuleEdit::doDefault', $params));
 
-$params = array('role' => array('client', 'staff'));
+//$params = array('role' => array('client', 'staff'));
 $routes->add('Rules Profile Settings', new \Tk\Routing\Route('/staff/ruleSettings.html', 'Rs\Controller\ProfileSettings::doDefault', $params));
 $routes->add('staff-rule-manager', new \Tk\Routing\Route('/staff/ruleManager.html', 'Rs\Controller\RuleManager::doDefault', $params));
 $routes->add('staff-rule-edit', new \Tk\Routing\Route('/staff/ruleEdit.html', 'Rs\Controller\RuleEdit::doDefault', $params));
