2d63be34a340bdf8c0d3930cf1498a4148b4f4b9
mifsudm <mifsudm@unimelb.edu.au>
2020-02-05 11:56:54 +1100

    ~Auto: Commit

--- Rs/Listener/SetupHandler.php
+++ Rs/Listener/SetupHandler.php
@@ -14,7 +14,7 @@ class SetupHandler implements Subscriber
 {
 
     /**
-     * @param \Symfony\Component\HttpKernel\Event\RequestEvent $event
+     * @param \Symfony\Component\HttpKernel\Event\GetResponseEvent $event
      * @throws \Tk\Db\Exception
      * @throws \Tk\Exception
      */
