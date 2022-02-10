61040492d74829c07efa5f7d46e77122d3a34b04
tropotek <info@tropotek.com>
2022-02-10 12:03:55 +1100

    ~Auto: Commit

---
+++ sql/mysql/00006.sql
@@ -0,0 +1,9 @@
+-- --------------------------------------------
+-- @version 3.2.48
+-- @author: Michael Mifsud <info@tropotek.com>
+--
+-- --------------------------------------------
+
+alter table rule add static TINYINT(1) default 1 not null after max;
+UPDATE rule SET static = 0 WHERE LENGTH(label) = 1 OR (label = 'small' OR label = 'prod' OR label = 'equine' OR label = 'other');
+
